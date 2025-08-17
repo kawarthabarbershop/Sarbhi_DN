1c6511442fe1e0ef810e450a6f433bc3d40f0dd5
kawarthabarbershop <melvin.f093@gmail.com>
2025-08-17 17:35:52 -0400

    Add files via upload

---
+++ typer.js
@@ -0,0 +1,8 @@
+
+    var typed = new Typed('#typer', {
+      strings: ['Tarot Card Reader', 'Psychic Angel Reader','Numerologist','Life Counselor'],
+      typeSpeed: 100,
+      backSpeed: 30,
+      backDelay: 1000,
+      loop: true
+    });
